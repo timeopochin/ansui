64daee36b33b1f8db33e1e3dbc11950d81a762af
Timeo Pochin <timeo.sam.pochin@gmail.com>
2020-12-10 10:29:22 +0100

    Added first running test for split function

---
+++ test/main.js
@@ -0,0 +1,5 @@
+let ansui = require('../lib/main');
+
+let test = ansui('yo, what is up b? u wanna do some cool integrationornay?sozmyspacekeybroken haha i am so cool\n\na\nb\nc', 8);
+
+console.log(test);
